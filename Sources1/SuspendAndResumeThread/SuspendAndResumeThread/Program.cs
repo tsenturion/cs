8c0233e22cf135c3c5cefcfbf9e9c67d1decd7a4
Паша Филиппов <golubchik191@gmail.com>
2025-01-22 20:20:12 +0300

    DONE

--- Sources1/SuspendAndResumeThread/SuspendAndResumeThread/Program.cs
+++ Sources1/SuspendAndResumeThread/SuspendAndResumeThread/Program.cs
@@ -5,27 +5,54 @@ namespace SuspendAndResumeThread
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            ThreadStart ts = new ThreadStart(Method);
-            Thread t = new Thread(ts);
-            t.Start(); // Запуск потока.
-            Console.WriteLine("Нажмите любую клавишу для остановки");
-            Console.ReadKey();
-            t.Suspend(); // Приостановка потока.
-            Console.WriteLine("Поток остановлен!");
-            Console.WriteLine("Нажмите любую клавишу для возобновления");
-            Console.ReadKey();
-            t.Resume(); // Возобновление работы.
-        }
+		static ManualResetEvent _pauseEvent = new ManualResetEvent(true); // Поток по умолчанию работает!!!!!
 
-        static void Method()
-        {
-            for (int i = 0; i < 100; i++)
-            {
-                Console.WriteLine(i);
-                Thread.Sleep(100);
-            }
-        }
-    }
+		static void Main(string[] args)
+		{
+			/*
+			* реализовать возможность множественной приостановки и продолжения!!!
+			* */ 
+			ThreadStart ts = new ThreadStart(Method);
+			Thread t = new Thread(ts);
+			t.Start(); 
+
+			Console.WriteLine("Нажмите 'p' для приостановки, 'r' для возобновления, 'e' для выхода.");
+			ConsoleKeyInfo key;
+			do
+			{
+				key = Console.ReadKey(true);
+				switch (key.KeyChar)
+				{
+					case 'p':
+					case 'P':
+						Console.WriteLine("Поток приостановлен.");
+						_pauseEvent.Reset(); // Ставим поток в режим ожидания
+						break;
+					case 'r':
+					case 'R':
+						Console.WriteLine("Поток возобновлен.");
+						_pauseEvent.Set(); // Поток может работать дальше
+						break;
+					case 'e':
+					case 'E':
+						Console.WriteLine("Завершение программы...");
+						_pauseEvent.Set(); // Позволяем потоку завершиться
+						break;
+				}
+			} while (key.KeyChar != 'e' && key.KeyChar != 'E');
+
+			t.Join(); // Дожидаемся завершения потока
+			Console.WriteLine("Программа завершена.");
+		}
+
+		static void Method()
+		{
+			for (int i = 0; i < 1000; i++)
+			{
+				_pauseEvent.WaitOne(); // Останавливаемся, если сигнал не установлен
+				Console.WriteLine(i);
+				Thread.Sleep(100);
+			}
+		}
+	}
 }
