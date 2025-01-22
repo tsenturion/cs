765030d8e3b48163e70965813eb344da95ba880f
Israpil1 <162831147+Israpil1@users.noreply.github.com>
2025-01-22 21:59:25 +0500

    pz1

---
+++ Sources1/SuspendAndResumeThread/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace SuspendAndResumeThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                ThreadStart ts = new ThreadStart(Method);
+                Thread t = new Thread(ts);
+                t.Start(); // Запуск потока.
+                Console.WriteLine("Нажмите любую клавишу для остановки");
+                Console.ReadKey();
+                t.Suspend(); // Приостановка потока.
+                Console.WriteLine("Поток остановлен!");
+                Console.WriteLine("Нажмите любую клавишу для возобновления");
+                Console.ReadKey();
+                t.Resume(); // Возобновление работы.
+            } while (true);
+        }
+
+        static void Method()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
