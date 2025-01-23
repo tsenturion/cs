aac293f71fe98440b42c5f4ff6b4890b3eae883c
Israpil1 <162831147+Israpil1@users.noreply.github.com>
2025-01-23 23:41:32 +0500

    Add files via upload

---
+++ hw1/hw1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MessageBoxApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button3.Text = "Показать сообщение";
+
+            button3.Click += new EventHandler(button3_Click);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string appName = "Привет, как дела?";
+            string appDescription = "Съешь этих французский булочек, да выпей чаю";
+
+            MessageBox.Show(appName, "Название приложения", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show(appDescription, "Описание приложения", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+    }
+}
