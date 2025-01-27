7d4c15437252e619cbbc02ac1c2cc268beddbf21
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-01-27 15:18:59 +0500

    all prev

--- Sources1/SuspendAndResumeThread/SuspendAndResumeThread/Program.cs
+++ Sources1/SuspendAndResumeThread/SuspendAndResumeThread/Program.cs
@@ -10,15 +10,25 @@ namespace SuspendAndResumeThread
             ThreadStart ts = new ThreadStart(Method);
             Thread t = new Thread(ts);
             t.Start(); // Запуск потока.
-            Console.WriteLine("Нажмите любую клавишу для остановки");
-            Console.ReadKey();
-            t.Suspend(); // Приостановка потока.
-            Console.WriteLine("Поток остановлен!");
-            Console.WriteLine("Нажмите любую клавишу для возобновления");
-            Console.ReadKey();
-            t.Resume(); // Возобновление работы.
+
+            ConsoleKey usersAns = ConsoleKey.G;
+
+            while (usersAns != ConsoleKey.Escape)
+            {
+                Console.WriteLine("Нажмите любую клавишу для остановки");
+                usersAns = Console.ReadKey().Key;
+                t.Suspend(); // Приостановка потока.
+                Console.WriteLine("Поток остановлен!");
+                Console.WriteLine("Нажмите любую клавишу для возобновления");
+                usersAns = Console.ReadKey().Key;
+                t.Resume(); // Возобновление работы.
+            }
+
+            t.Abort();
+
         }
 
+  
         static void Method()
         {
             for (int i = 0; i < 100; i++)
