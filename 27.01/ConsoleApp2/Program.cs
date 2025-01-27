3a6c0bf5bd6c47a373349506f24fb11419891cfe
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-01-27 21:54:30 +0500

    pz 1 done

---
+++ 27.01/ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<object> collection = new List<object>
+        {
+            42,
+            "Hello, World!",
+            3.14,
+            DateTime.Now
+        };
+
+        Thread thread = new Thread(() => ProcessCollection(collection));
+
+        thread.Start();
+
+        thread.Join();
+
+        Console.WriteLine("Поток завершил работу.");
+    }
+
+    static void ProcessCollection(IEnumerable<object> collection)
+    {
+        foreach (var item in collection)
+        {
+            Console.WriteLine(item.ToString());
+        }
+    }
+}
