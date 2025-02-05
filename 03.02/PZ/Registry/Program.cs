01fc05852460bae2edd3735dcc3cc2b46b071e73
Agash_hk16@top-academy.ru <Agash_hk16@top-academy.ru>
2025-02-05 19:18:47 +0300

    PZ

---
+++ 03.02/PZ/Registry/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Win32;
+
+namespace Program
+{
+	class Program
+	{
+		static void Main()
+		{
+			RegistryKey regSoftware = Registry.CurrentUser.CreateSubKey("Software");
+			RegistryKey MyRegTest = regSoftware.CreateSubKey("MyRegistryTest");
+			MyRegTest.SetValue("", "Это тестовый ключ");
+			MyRegTest.SetValue("TestString", "Hell o, world!", RegistryValueKind.String);
+			MyRegTest.SetValue("TestInt", 42, RegistryValueKind.DWord);
+
+			Console.WriteLine(MyRegTest.GetValue(""));
+			Console.WriteLine(MyRegTest.GetValue("TestString"));
+			Console.WriteLine(MyRegTest.GetValue("TestInt"));
+			if(MyRegTest.GetValue("1") == null) Console.WriteLine("Значение отсутствует!!!!!!!!!!11");
+
+			MyRegTest.SetValue("TestString", "Изменённое значение");
+			MyRegTest.SetValue("TestInt", 100);
+
+			regSoftware.DeleteSubKey("MyRegistryTest");
+		}
+	}
+}
---
+++ 03.02/PZ/Tasks/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Program
+{
+	class Program
+	{
+		static void Main()
+		{
+			#region Task1
+			//Action action = new Action(delegate 
+			//{
+			//	for (int i = 0; i < 100; i++)
+			//	{
+			//		Console.WriteLine($"\tTask2: {DateTime.Now.ToString()}");
+			//		Thread.Sleep(100);
+			//	}
+			//});
+			//Task task1 = new Task(delegate 
+			//{
+			//	for (int i = 0; i < 100; i++)
+			//	{
+			//		Console.WriteLine($"Task1: {DateTime.Now.ToString()}");
+			//		Thread.Sleep(100);
+			//	}
+			//});
+			//task1.Start();
+
+			//Task task2 = Task.Factory.StartNew(action);
+
+			//Task task3 = Task.Run(delegate
+			//{
+			//	for (int i = 0; i < 100; i++)
+			//	{
+			//		Console.WriteLine($"\t\tTask3: {DateTime.Now.ToString()}");
+			//		Thread.Sleep(100);
+			//	}
+			//});
+
+			//Console.ReadKey(); 
+			#endregion
+
+			#region Task2
+			//{
+			//	Task task = Task.Run(delegate
+			//	{
+			//		for(int i=2;i<1000;i++)
+			//		{
+			//			if(i % i + 1 != 0 && i % i + 2 != 0&& i % i + 3 != 0)
+			//			Console.WriteLine(i + "\t");
+			//		}
+			//	});
+			//}
+			//Console.ReadKey();
+			#endregion
+
+			#region Task4
+			{
+
+			}
+			#endregion
+			//Task.WaitAll();
+		}
+	}
+}
