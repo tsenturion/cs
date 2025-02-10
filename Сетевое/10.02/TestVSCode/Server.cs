22d5ec12a08914253ec7e3c0c46ac4eb6f1776ff
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-02-10 23:10:00 +0500

    all

---
+++ Сетевое/10.02/TestVSCode/Server.cs
@@ -0,0 +1,32 @@
+﻿using System.Net
+using System.Net.Socet
+using System
+using System.Text
+
+namespace TestVSCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string iPAddress = "127.0.0.1";
+            int port = 1024;
+
+            Socket serverSocket = new Socket(AddreSSFamily.InterNetwork, SocketType.Stream, ProtocalType.Tcp);
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse(iPAddress), port);
+            serverSocket.Bind(ipEndPoint);
+
+            serverSocket.Listen(1);
+            Console.WriteLine("Ожидание подключение клиента");
+
+            Socket clientSocket = serverSocket.Accept();
+            Console.WriteLine("Клиент подключен" + clientSocket.RemoteEndPoint.ToString());
+
+            byte[] buffer = new byte[1024];
+            int receivedBytes = clientSocket.Receive(buffer);
+            string receivedMessage = Encoding.ASCII.GetString(buffer, 0, receivedBytes);
+
+            Console.WriteLine($"Сервер:\nD {DateTime.Now:HH:mm} от");
+        }
+    }
+}
--- Сетевое/1_2/TestUDP/Form1.cs
+++ Сетевое/1_2/TestUDP/Form1.cs
@@ -13,49 +13,64 @@ namespace TestUDP
 {
     public partial class Form1 : Form
     {
-       
+        // Делегат для безопасного обновления текстового поля из другого потока
         delegate void AddTextDelegate(String text);
+
+        // Поток для приема сообщений
         System.Threading.Thread thread;
+
+        // Сокет для UDP-соединения
         Socket socket;
 
         public Form1()
         {
             InitializeComponent();
         }      
+
+        // Метод для добавления текста в textBox1
         void AddText(String text)
         {
-            textBox1.Text+=text;
+            textBox1.Text += text;
         }
+
+        // Функция приема сообщений, выполняемая в отдельном потоке
         void RecivFunction(object obj)
         {
-            Socket rs=(Socket) obj;
+            Socket rs = (Socket)obj;
 
             byte[] buffer = new byte[1024];
             do
             {
                 EndPoint ep = new IPEndPoint(0x7F000000, 100);
-                int l=rs.ReceiveFrom(buffer, ref ep);
-                String strClientIP=((IPEndPoint)ep).Address.ToString(); 
+                int l = rs.ReceiveFrom(buffer, ref ep);
+                String strClientIP = ((IPEndPoint)ep).Address.ToString();
                 String str = String.Format("\nReceived from {0}\r\n{1}\r\n", 
                     strClientIP, System.Text.Encoding.Unicode.GetString(buffer, 0, l));
                 textBox1.BeginInvoke(new AddTextDelegate(AddText), str);
             } while (true);
         }
+
+        // Обработчик нажатия кнопки "Start"
         private void button1_Click(object sender, EventArgs e)
         {
-            if (socket != null &&thread!=null)
+            if (socket != null && thread != null)
                 return;
-            socket=new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.IP);
+
+            // Создание и настройка сокета
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.IP);
             socket.Bind(new IPEndPoint(IPAddress.Parse("10.2.21.129"), 100));
 
+            // Запуск потока для приема сообщений
             thread = new System.Threading.Thread(RecivFunction);
             thread.Start(socket);
-
         }
+
+        // Обработчик нажатия кнопки "Stop"
         private void button2_Click(object sender, EventArgs e)
         {
             if (socket != null)
             {               
+                // Остановка потока и закрытие сокета
                 thread.Abort();
                 thread = null;
                 socket.Shutdown(SocketShutdown.Receive);
@@ -64,13 +79,17 @@ namespace TestUDP
                 textBox1.Text = "";
             }
         }
+
+        // Обработчик нажатия кнопки "Clear"
         private void button3_Click(object sender, EventArgs e)
         {
             textBox1.Text = "";
         }
 
+        // Обработчик нажатия кнопки "Send"
         private void button4_Click(object sender, EventArgs e)
         {
+            // Отправка сообщения через UDP
             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.IP);
             socket.SendTo(System.Text.Encoding.Unicode.GetBytes(textBox2.Text), new IPEndPoint(IPAddress.Parse("10.2.21.255"), 100));
             socket.Shutdown(SocketShutdown.Send);
@@ -78,3 +97,4 @@ namespace TestUDP
         }
     }
 }
+
