dff39c55651387a3036bf39622d43614acb0367d
Agash_hk16@top-academy.ru <Agash_hk16@top-academy.ru>
2025-02-10 23:50:02 +0300

    Сетевое программирование , ресурсы

---
+++ Сетевое/1_2/TestServerAsyncSocket/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TestServerAsyncSocket
+{
+    class AsyncServer
+    {
+        
+        IPEndPoint endP;
+        Socket socket;        
+
+        public AsyncServer(string strAddr, int port)
+        {
+            endP = new IPEndPoint(IPAddress.Parse(strAddr), port);
+        }
+        void MyAcceptCallbakFunction(IAsyncResult ia)
+        {
+            //get a link to the listening socket
+            Socket socket=(Socket)ia.AsyncState;
+           
+            //get a socket to exchange data with the client
+            Socket ns = socket.EndAccept(ia);
+            
+            //output the connection information to the console
+            Console.WriteLine(ns.RemoteEndPoint.ToString());
+            
+            //send the client the current time asynchronously, 
+            //by the end of the sending operation, the  
+            //MySendCallbackFunction method will be called.
+            byte[] sendBufer = System.Text.Encoding.ASCII.GetBytes(DateTime.Now.ToString());
+            ns.BeginSend(sendBufer, 0, sendBufer.Length, SocketFlags.None, new AsyncCallback(MySendCallbackFunction), ns);
+            
+            //resume asynchronous Accept
+            socket.BeginAccept(new AsyncCallback(MyAcceptCallbakFunction), socket);
+        }
+        void MySendCallbackFunction(IAsyncResult ia)
+        { 
+            //after sending data to the client,
+            //close the socket (if we would need to continue
+            //data exchange, we could have arranged it here)
+            Socket ns=(Socket)ia.AsyncState;
+            int n = ((Socket)ia.AsyncState).EndSend(ia);
+            ns.Shutdown(SocketShutdown.Send);
+            ns.Close();
+        }
+
+        public void StartServer()
+        {
+            if (socket != null)
+                return;
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            socket.Bind(endP);
+            socket.Listen(10);
+            //start asynchronous Accept, when the client 
+            //is connected, the MyAcceptCallbakFunction handler is called.
+            socket.BeginAccept(new AsyncCallback(MyAcceptCallbakFunction), socket);
+        }
+    }
+
+
+    class Program
+    {     
+        static void Main(string[] args)
+        {
+            AsyncServer server = new AsyncServer("127.0.0.1", 1024);
+            server.StartServer();
+            Console.Read();         
+        }
+    }
+}
---
+++ Сетевое/4/TcpSample/Server/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        TcpListener list;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //creating the TcpListener class instance
+                //data on host and port are read
+                //from text boxes
+                list = new TcpListener(
+                    IPAddress.Parse(textBox1.Text),
+                    Convert.ToInt32(textBox2.Text));
+                //start listening to clients
+                list.Start();
+                //creating a separate thread to read messages
+                Thread thread = new Thread(
+                        new ThreadStart(ThreadFun)
+                        );
+                thread.IsBackground = true;
+                //start a thread
+                thread.Start();
+            }
+            catch (SocketException sockEx)
+            {
+                MessageBox.Show("Socket exception: " + sockEx.Message);
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show("Exception : " + Ex.Message);
+            }
+        }
+
+        void ThreadFun()
+        {
+            while (true)
+            {
+                //server informs a client that it is ready 
+                //for connection
+                TcpClient cl = list.AcceptTcpClient();
+                //reading data from network in the Unicode format
+                StreamReader sr = new StreamReader(
+                    cl.GetStream(), Encoding.Unicode
+                );
+                string s = sr.ReadLine();
+                //adding the obtained message to the list
+                messageList.Items.Add(s);
+                cl.Close();
+                //when getting the EXIT message, exit the application
+                if (s.ToUpper() == "EXIT")
+                {
+                    list.Stop();
+                    this.Close();
+                }
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (list != null)
+                list.Stop();
+        }
+    }
+}
