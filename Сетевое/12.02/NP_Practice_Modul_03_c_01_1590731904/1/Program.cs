1d273636c1ac73851ef7dc5c2b25cbe5cc1de7b1
Agash_hk16@top-academy.ru <Agash_hk16@top-academy.ru>
2025-02-12 23:49:23 +0300

    PZ 1 in progress

---
+++ Сетевое/12.02/NP_Practice_Modul_03_c_01_1590731904/1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics.Metrics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Program
+{
+	class Server
+	{
+		static void Main()
+		{
+			Random r = new();
+			string[] quotes = {
+				"\r\nUnderidoderidoderiododeridoo\r\n",
+				"\r\nNever yield to force\r\n",
+				"\r\nNever yield to the apparently overwhelming might of the enemy\r\n",
+				"\r\nI love spaghetti\r\n"
+			};
+
+			Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+			IPEndPoint IPEnd = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1025);
+			serverSocket.Bind(IPEnd);
+			serverSocket.Listen(10);
+
+			Socket clientSocket = serverSocket.Accept();
+			Console.WriteLine($"New connection: {clientSocket.RemoteEndPoint}");
+			clientSocket.Send(Encoding.ASCII.GetBytes("1 to get quote, 0 to close connection\r\n"));
+			byte[] buffer = new byte[1024];
+			bool flag = true;
+			while(flag)
+			{
+				clientSocket.Receive(buffer);
+				string query = Encoding.ASCII.GetString(buffer);
+				switch(int.Parse(query))
+				{
+					case 1:
+						{
+							clientSocket.Send(Encoding.ASCII.GetBytes(quotes[r.Next(quotes.Length)]));
+							break;
+						}
+					case 0:
+						{
+							clientSocket.Shutdown(SocketShutdown.Both);
+							clientSocket.Close();
+							flag = false;
+							break;
+						}
+					default:
+						{
+							Console.WriteLine(query);
+							break;
+						}
+				}
+			}
+			
+		}
+	}
+}
---
+++ Сетевое/12.02/NP_Practice_Modul_03_c_01_1590731904/1_client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+	class Client
+	{
+		static void Main()
+		{
+			//Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+			//IPEndPoint IPEnd = new(IPAddress.Parse("127.0.0.1"), 1024);
+			//clientSocket.Bind(IPEnd);
+			Random r = new();
+			for (int i=0;i<10;i++)
+			{
+				ThreadPool.QueueUserWorkItem(StartClient);
+				Thread.Sleep(r.Next(500));
+			}
+
+			
+		}
+		static void StartClient(object obj)
+		{
+			Random r = new();
+
+			Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+			serverSocket.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1025));
+			//clientSocket.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1025));
+			while (!serverSocket.Connected) Thread.Sleep(50);
+			while (serverSocket.Connected)
+			{
+				serverSocket.Send(Encoding.ASCII.GetBytes("1"));
+				byte[] buffer = new byte[1024];
+				serverSocket.Receive(buffer);
+				Console.WriteLine(Encoding.ASCII.GetString(buffer));
+				Thread.Sleep(r.Next(1000));
+			}
+		}
+	}
+}
