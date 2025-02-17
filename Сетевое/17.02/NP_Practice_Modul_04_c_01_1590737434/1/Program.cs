1702cf54d66b26ae9fe8bcd7287d27d593553292
Agash_hk16@top-academy.ru <Agash_hk16@top-academy.ru>
2025-02-17 22:54:17 +0300

    Task 1 Done

---
+++ Сетевое/17.02/NP_Practice_Modul_04_c_01_1590737434/1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MulticastServer
+{
+	public class MulticastServer
+	{
+		static string message = "Hello network!!!";
+		static int Interval = 1000;
+		static void multicastSend()
+		{
+			while (true)
+			{
+				Thread.Sleep(Interval);
+				Socket soc = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+				soc.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 2);
+				IPAddress dest = IPAddress.Parse("224.5.5.5");
+				soc.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(dest));
+				IPEndPoint ipep = new IPEndPoint(dest, 4567);
+				soc.Connect(ipep);
+				soc.Send(Encoding.Default.GetBytes(message));
+				soc.Close();
+			}
+		}
+		void SendMulticastMessage(string text)
+		{
+			Socket socket = new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+			socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 2);
+			IPAddress destination = IPAddress.Parse("224.5.5.5");
+			socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(destination));
+			IPEndPoint IPEnd = new(destination, 4567);
+			socket.Connect(IPEnd);
+			socket.Send(Encoding.UTF8.GetBytes(text));
+			socket.Close();
+		}
+		Thread Sender = new Thread(new ThreadStart(multicastSend));
+		public MulticastServer()
+		{
+			//Sender.IsBackground = true;
+			//Sender.Start();
+		}
+
+		static void Main()
+		{
+			MulticastServer server = new();
+
+			string message = ""; 
+			message = Console.ReadLine();
+			server.SendMulticastMessage(message);
+		}
+	}
+}
---
+++ Сетевое/17.02/NP_Practice_Modul_04_c_01_1590737434/1_client/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace MulticastClient
+{
+	public class MulticastClient
+	{
+		void Listener()
+		{
+			while (true)
+			{
+				Socket soc = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+				IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 4567);
+				soc.Bind(ipep);
+				IPAddress ip = IPAddress.Parse("224.5.5.5");
+				soc.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(ip, IPAddress.Any));
+				soc.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(ip, IPAddress.Any));
+				byte[] buff = new byte[1024];
+				soc.Receive(buff);
+				Console.WriteLine(Encoding.UTF8.GetString(buff) + "\n");
+				soc.Close();
+			}
+		}
+		Thread listen;
+		public MulticastClient()
+		{
+			listen = new Thread(new ThreadStart(Listener));
+			listen.IsBackground = true;
+			listen.Start();
+		}
+		static void Main()
+		{
+			MulticastClient client = new();
+			Thread.Sleep(120000);
+		}
+	}
+}
