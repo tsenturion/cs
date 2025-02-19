a2b7b1ebd59e7cb5553760728fe1a1816cae09f7
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-02-19 20:52:52 +0500

    pz1 done

--- Сетевое/10.02/TestVSCode/Server.cs
+++ Сетевое/10.02/TestVSCode/Server.cs
@@ -4,104 +4,45 @@ using System.Net.Http.Headers;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
 
 namespace TestVSCode
 {
     class Program
     {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("любую клавишу");
+            Console.ReadKey();
+            string hamletText = await DownloadHamletAsync();
+            Console.Clear();
+            Console.WriteLine("загружено");
+            Console.WriteLine(hamletText);
+        }
+        static async Task<string> DownloadHamletAsync()
+        {
+            string habletUrl = "https://www.gutenberg.org/files/1524/1524-0.txt";
 
-        static string infoMessage = "infoMessage";
-        static string wariningMessage = "wariningMessage";
-        static string urgentMessage = "urgentMessage";
-        static Dictionary<Socket, List<MessageType>> clientSubscriptions = new Dictionary<Socket, List<MessageType>>();
-        enum MessageType 
-        {
-            Information,
-            Warning,
-            Urgent
-        }
-        static List<Socket> clients = new List<Socket>();
-        static int interval = 5000;
-        static void Main(string[] args)
-        {
-            Socket listenerSocket = new Socket(AddressFamily.InterNetwork,SocketType.Dgram, ProtocolType.Udp);
-            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 4567);
-            listenerSocket.Bind(localEndPoint);
-            Console.WriteLine("server started");
-            Thread clienListenerThread = new Thread (() =>
+            using (HttpClient client = new HttpClient())
             {
-                while (true)
+                try
                 {
-                    Socket clientSocket = listenerSocket.Accept();
-                    List<MessageType> subscription = new List<MessageType> { MessageType.Information, MessageType.Warning};
-                    SubscribeClient(clientSocket, subscription);
-                    Console.WriteLine("client subscribed");
-                    SendMessageToClient(clientSocket, infoMessage);
-                    SendMessageToClient(clientSocket, wariningMessage);
-                    SendUrgentMessage(urgentMessage);
+                    HttpResponseMessage response = await client.GetAsync(habletUrl);
+                    response.EnsureSuccessStatusCode();
+                    string hamletText = await response.Content.ReadAsStringAsync();
+                    return hamletText;
                 }
-            });
-            clienListenerThread.Start();
-            while (true)
-            {
-                Console.WriteLine("clientSocket to remove or exit");
-                string input = Console.ReadLine();
-                if (input == "exit")
+                catch (HttpRequestException e)
                 {
-                    break;
+                    Console.WriteLine($"Ошибка при загрузке «Гамлета»: {e.Message}");
+                    return "Не удалось загрузить «Гамлета». Проверьте подключение к Интернету.";
                 }
-                if (RemoveClintFromSubscription(input))
-                {
-                    Console.WriteLine("client removed");
-                }
-                else{
-                    Console.WriteLine("client not found");
-                }
-
-            }
-        
-        }
-        static bool RemoveClintFromSubscription(string clientAddress)
-        {
-            foreach (var client in clientSubscriptions.Keys)
-            {
-                IPEndPoint clientEndPoint = (IPEndPoint)client.RemoteEndPoint;
-                if (clientEndPoint.Address.ToString() == clientAddress)
-                {
-                    clientSubscriptions.Remove(client);
-                    client.Close();
-                    return true;
-                }
-            }
-            return false;
-        }
-        
-        static void SendMessageToClient(Socket clientSocket, string message)
-        {
-            List<MessageType> subsctiptions = clientSubscriptions[clientSocket];
-            if (message == "urgentMessage" || subsctiptions.Contains(MessageType.Information))
-            {
-                byte[] messageBytes = Encoding.Default.GetBytes(message);
-                clientSocket.Send(messageBytes);
-                Console.WriteLine($"sent message {message} to client");
             }
         }
 
-        static void SubscribeClient(Socket clientSocket, List<MessageType> subsctiptionTypes)
-        {
-            clientSubscriptions[clientSocket] = subsctiptionTypes;
-        }
 
-        static void SendUrgentMessage(string urgentMessage)
-        {
-            byte[] messageBytes = Encoding.Default.GetBytes(urgentMessage);
-
-            foreach (var clint in clientSubscriptions.Keys)
-            {
-                clint.Send(messageBytes);
-            }
-            Console.WriteLine("Sent urgent message for all");
-        }
-            
     }
-}
+ }
