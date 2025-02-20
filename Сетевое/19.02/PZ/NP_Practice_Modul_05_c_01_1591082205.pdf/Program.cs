56f5bfb0a02010c4d80ec0dd2c27d00a2295e76f
Agash_hk16@top-academy.ru <Agash_hk16@top-academy.ru>
2025-02-20 21:51:44 +0300

    pz1 DONE

---
+++ Сетевое/19.02/PZ/NP_Practice_Modul_05_c_01_1591082205.pdf/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+
+namespace Program
+{
+	class Program
+	{
+		static async Task Main()
+		{
+			Console.WriteLine("Загрузка...");
+			string hamletText = await DownloadHamletText();
+			Console.Clear();
+			Console.WriteLine(hamletText);
+			Console.ReadKey();
+			Console.Clear();
+		}
+		static async Task<string> DownloadHamletText()
+		{
+			string hamletURL = "https://www.gutenberg.org/files/1524/1524-0.txt";
+			using (HttpClient client = new())
+			{
+				try
+				{
+					HttpResponseMessage response = await client.GetAsync(hamletURL);
+					response.EnsureSuccessStatusCode();
+					string hamletText = await response.Content.ReadAsStringAsync();
+					return hamletText;
+				}
+				catch(HttpRequestException e)
+				{
+					Console.WriteLine("Ошибка: " + e.Message);
+					return "Не удалось";
+				}
+			}
+		}
+	}
+}
