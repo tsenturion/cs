4d29e365813ff3262160408fd7975afff9071c36
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-02-21 22:58:21 +0500

    pz 3 done

--- Сетевое/10.02/TestVSCode/Server.cs
+++ Сетевое/10.02/TestVSCode/Server.cs
@@ -7,103 +7,47 @@ using HtmlAgilityPack;
 using System.Web;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Net;
+using System.IO;
 
 namespace TestVSCode
 {
     class Program
-    {
-        private static readonly HttpClient client = new HttpClient();
-
-        private const string apiKey = "";        
-        static async Task Main(string[] args)
+    {     
+        static void Main(string[] args)
         {
-            Console.WriteLine("1 - человек / 2 - бот");
-            string choice = Console.ReadLine();
+            string url = "https://www.gutenberg.org/";
+            string searchText = "book";
 
-            if (choice == "1")
+            try
             {
-                await PlayerVsPlayer();
-            }
-            else if (choice == "2") {
-                await BotVsPlayer();
-            }
-            else{
-                Console.WriteLine("Неправильный ввод. Попробуйте еще раз.");
-            }
-            Console.ReadKey();
-        }
-        
-        private static async Task PlayerVsPlayer()
-        {
-            Console.WriteLine("человек человек");
-            Console.WriteLine("для броска человек 1 enter");
-            Console.ReadLine();
-            int player1roll = await RollDice();
-            Console.WriteLine($"Кубик выпал: {player1roll}");
-            Console.WriteLine("для броска человек 2 enter");
-            Console.ReadLine();
-            int player2roll = await RollDice();
-            Console.WriteLine($"Кубик выпал: {player2roll}");
-            DetermineWinner(player1roll, player2roll);
-        }
-        
-        private static async Task<int> RollDice()
-        {
-            JObject requestParams = new JObject
-            {
-                ["apiKey"] = apiKey,
-                ["n"] = 1,
-                ["min"] = 1,
-                ["max"] = 6,
-                ["replacement"] = true
-            };
-            JObject requestBody = new JObject
-            {
-                ["jsonrpc"] = "2.0",
-                ["method"] = "generateIntegers",
-                ["params"] = requestParams,
-                ["id"] = 1
-            };
+                WebRequest request = WebRequest.Create(url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
 
-            string jsonBody = requestBody.ToString(Formatting.None);
-
-            var response = await client.PostAsync(
-                "https://api.random.org/json-rpc/4/invoke",
-                new StringContent(jsonBody, Encoding.UTF8, "application/json")
-            );
-
-            var resultJson = await response.Content.ReadAsStringAsync();
-            dynamic result = JsonConvert.DeserializeObject(resultJson);
-            return result.result.random.data[0];
-        }
-        
-        private static void DetermineWinner(int roll1, int roll2)
-        {
-            if (roll1 > roll2)
-            {
-                Console.WriteLine("первый выиграл");
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                string html = reader.ReadToEnd();
+                if (html.Contains(searchText))
+                {
+                    Console.WriteLine("Text found on the page.");
+                    int index = html.IndexOf(searchText);
+                    int fragmentStart = Math.Max(0, index - 50);
+                    int fragmentEnd = Math.Min(100, html.Length - fragmentStart);
+                    string fragment = html.Substring(fragmentStart, fragmentEnd);
+                    Console.WriteLine($"Fragment: {fragment}");
+                }
+                else
+                {
+                    Console.WriteLine("Text not found on the page.");
+                }
+                reader.Close();
+                dataStream.Close();
+                response.Close();
             }
-            else if (roll1 < roll2)
+            catch (Exception e)
             {
-                Console.WriteLine("второй выиграл");
+                Console.WriteLine("Error: " + e.Message);
             }
-            else
-            {
-                Console.WriteLine("ничья");
-            }
-        }
-        
-        private static async Task BotVsPlayer()
-        {
-            Console.WriteLine("человек бот");
-            Console.WriteLine("для броска человека 1 enter");
-            Console.ReadLine();
-            int player1roll = await RollDice();
-            Console.WriteLine($"Кубик выпал: {player1roll}");
-            Random random = new Random();
-            int botRoll = random.Next(1, 7);
-            Console.WriteLine($"Кубик выпал бота: {botRoll}");
-            DetermineWinner(player1roll, botRoll);
         }
     }
 }
