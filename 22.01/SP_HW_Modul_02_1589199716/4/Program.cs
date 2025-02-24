68cc01c886a5910c013ae1b87cc4eeb73a20c7f7
Agash_hk16@top-academy.ru <Agash_hk16@top-academy.ru>
2025-02-24 19:31:40 +0300

    task 4 done

---
+++ 22.01/SP_HW_Modul_02_1589199716/4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace Program
+{
+	class Program
+	{
+		static void Main()
+		{
+			string path = "";
+			while(true)
+			{
+				Console.WriteLine("Задайте путь к файлу: ");
+				path = Console.ReadLine();
+				if (Path.Exists(path)) break;
+				else Console.WriteLine("Некорректный путь");
+			}
+			Console.WriteLine("Задайте ключевое слово: ");
+			string userWord = Console.ReadLine();
+			StreamReader sr = new(path);
+			string[] text = sr.ReadToEnd().Split([' ', ',', '!', '?', '.', ';']);
+			int wordsCount = 0;
+			foreach(string word in text)
+				if (word == userWord) wordsCount++;
+			Console.WriteLine("Таких слов " + wordsCount);
+		}
+	}
+}
