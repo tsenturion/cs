9fc51725ccae5d123f916fa31da8a429d64abcf7
Agash_hk16@top-academy.ru <Agash_hk16@top-academy.ru>
2025-02-24 19:14:41 +0300

    task 1 done

---
+++ 22.01/SP_HW_Modul_02_1589199716/1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace Program
+{
+	class Program
+	{
+		static void Main()
+		{
+			Process process = new();
+			process.StartInfo = new ProcessStartInfo("notepad.exe");
+			Console.WriteLine("Процесс запущен");
+			process.Start();
+			process.WaitForExit();
+			Console.WriteLine($"Процесс остановлен, код процесса: {process.GetHashCode()}");
+		}
+	}
+}
