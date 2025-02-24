1e38a5f702b8f300da988277dc6a48724db672da
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-02-24 22:54:47 +0500

    pz1 done

--- Сетевое/10.02/TestVSCode/Server.cs
+++ Сетевое/10.02/TestVSCode/Server.cs
@@ -16,116 +16,132 @@ namespace TestVSCode
     {     
         static async Task Main(string[] args)
         {
-            string url = "https://www.gutenberg.org/";
-            string searchText = "book";
-            int maxTimeSeconds = 10;
-            int maxConcurrency = 5;
+            string city = "Москва";
+            string apiKey = "3fded8a47f3c2bbddc3af0cce9a3120b";
+            string url = $"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric";
+            try{
+                var weatherData = await GetWeatherData(url);
+                double lat = weatherData.coord.lat;
+                double lon = weatherData.coord.lon;
+                //string oneCallUrl = $"http://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=hourly,minutely&appid={apiKey}&units=metric";
+                string oneCallUrl = $"http://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={apiKey}&units=metric";
+                var currentWeather = await GetCurrentWeather(oneCallUrl);
 
-            var cts = new CancellationTokenSource(TimeSpan.FromSeconds(maxTimeSeconds));
-            var semaphore = new SemaphoreSlim(maxConcurrency);
-            var urlsToParse = new List<string> { url };
-            var results = new List<string>();
-            Console.WriteLine(" + для добавления, - для удаления, c - cansel, p - пауза, r - resume");
-            bool isPaused = false;
-            var tasks = new List<Task>();
-            while (urlsToParse.Count > 0 || tasks.Count > 0)
-            {
-                if (urlsToParse.Count > 0  && semaphore.CurrentCount > 0 && !isPaused)
-                {
-                    var currentUrl = urlsToParse[0];
-                    urlsToParse.RemoveAt(0);
-                    tasks.Add(ParseUrlAsync(currentUrl, semaphore, cts.Token, results, searchText));
-                    Console.WriteLine($"Парсинг {currentUrl}");
-                }
-                var key = Console.ReadKey(true);
-                switch (key.KeyChar.ToString().ToLower())
-                {
-                    case "+":
-                        if (maxConcurrency < 10)
-                        {
-                            maxConcurrency++;
-                            semaphore.Release();
-                            Console.WriteLine($"Max concurrency increased to {maxConcurrency}");
-                        }
-                        break;
-                    case "-":
-                        if (maxConcurrency > 1)
-                        {
-                            maxConcurrency--;
-                            Console.WriteLine($"concurrency decreased to {maxConcurrency}");
-                        }
-                        break;
-                    case "c":
-                        cts.Cancel();
-                        Console.WriteLine("Cancelling.");
-                        return;
-                    case "p":
-                        isPaused = true;
-                        Console.WriteLine("Paused.");
-                        break;
-                    case "r":
-                        isPaused = false;
-                        Console.WriteLine("Resumed.");
-                        break;
-                    default:
-                        break;
-                }
-                await Task.Delay(100);
+                Console.WriteLine($"City: {weatherData.name}, Temperature: {currentWeather.current.temp}°C, Humidity: {currentWeather.current.humidity}% описание {currentWeather.current.weather[0].description}");
             }
-            foreach (var result in results)
+            catch (HttpRequestException ex)
             {
-                Console.WriteLine(result.ToString());
-            }
-        }
-        
-        static async Task ParseUrlAsync(string url, SemaphoreSlim semaphore, CancellationToken cancellationToken, List<string> results, string searchText)
-        {
-            await semaphore.WaitAsync(cancellationToken);
-            try
-            {
-                using var httpClient = new HttpClient();
-                var response = await httpClient.GetAsync(url, cancellationToken);
-                if (response.IsSuccessStatusCode)
-                {
-                    var html = await response.Content.ReadAsByteArrayAsync(cancellationToken);
-                    var htmlString = Encoding.UTF8.GetString(html);
-                    if (htmlString.Contains(searchText))
-                    {
-                        int index = htmlString.IndexOf(searchText);
-                        int fragmentStart = Math.Max(0, index - 50);
-                        int fragmentEnd = Math.Min(100, htmlString.Length - fragmentStart);
-                        string fragment = htmlString.Substring(fragmentStart, fragmentEnd);
-                        results.Add($"{url} - {fragment}");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"Failed to get page: {url} - {response.StatusCode}");
-                }
-            }
-            catch (OperationCanceledException ex)
-            {
-                Console.WriteLine($"Task cancelled: {ex.Message}");
+                Console.WriteLine($"Error connecting to the API: {ex.Message}");
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"An error occurred: {ex.Message}");
+                Console.WriteLine($"Error: {ex.Message}");
             }
-            finally
+        }
+
+        static async Task<WeaterResponse> GetWeatherData(string url)
+        {
+            using var client = new HttpClient();
+            var response = await client.GetAsync(url);
+            if (!response.IsSuccessStatusCode)
             {
-                semaphore.Release();
+                throw new HttpRequestException($"Request failed with status code: {response.StatusCode}");
             }
+            var data = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<WeaterResponse>(data);
+        }
+        
+        static async Task<OneCallResponse> GetCurrentWeather(string url)
+        {
+            using var client = new HttpClient();
+            var response = await client.GetAsync(url);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new HttpRequestException($"Request failed with status code: {response.StatusCode}");
+            }
+            var data = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<OneCallResponse>(data);
         }
     }
     
-    public class SearchResult
+    public class Coord
     {
-        public string Url { get; set; }
-        public string Fragment { get; set; }
-        
-        public override string ToString()
-        {
-            return $"URL: {Url}\nFragment: {Fragment}\n";
-        }
+        public double lon { get; set; }
+        public double lat { get; set; }
+    }
+    
+    public class Weather
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+    
+    public class Main
+    {
+        public double temp { get; set; }
+        public double feels_like { get; set; }
+        public double temp_min { get; set; }
+        public double temp_max { get; set; }
+        public int pressure { get; set; }
+        public int humidity { get; set; }
+    }
+    
+    public class Sys
+    {
+        public int type { get; set; }
+        public int id { get; set; }
+        public string country { get; set; }
+        public int sunrise { get; set; }
+        public int sunset { get; set; }
+        public double message { get; set; }
+    }
+
+    public class WeaterResponse
+    {
+        public int id { get; set; }
+        public List<Weather> weather { get; set; }
+        public string @base { get; set; }
+        public Main main { get; set; }
+        public int visibility { get; set; }
+        public int dt { get; set; }
+        public Sys sys { get; set; }
+        public int timezone { get; set; }
+        public string name { get; set; }
+        public Coord coord { get; set; }
+        public int cod { get; set; }
+
+        //public int country { get; set; }
+        //public int population { get; set; }
+    }
+    
+    public class OneCallResponse
+    {
+        public double lat { get; set; }
+        public double lon { get; set; }
+        public string timezone { get; set; }
+        public int timezone_offset { get; set; }
+        public Current current { get; set; }
+        //public int dt { get; set; }
+        //public List<Weather> current { get; set; }
+    }
+
+    public class Current
+    {
+        public int dt { get; set; }
+        public int sunrise { get; set; }
+        public int sunset { get; set; }
+        public double temp { get; set; }
+        public double feels_like { get; set; }
+        public int pressure { get; set; }
+        public int humidity { get; set; }
+        public double dew_point { get; set; }
+        public double uvi { get; set; }
+        public int clouds { get; set; }
+        public int visibility { get; set; }
+        public double wind_speed { get; set; }
+        public int wind_deg { get; set; }
+        public List<Weather> weather { get; set; }
     }
 }
