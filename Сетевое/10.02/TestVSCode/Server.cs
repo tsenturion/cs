a8e84211bb77cd872702806240c19fe3210fb04d
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-02-26 21:43:04 +0500

    pz 3 done

--- Сетевое/10.02/TestVSCode/Server.cs
+++ Сетевое/10.02/TestVSCode/Server.cs
@@ -4,63 +4,53 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using HtmlAgilityPack;
 
 namespace TestVSCode
 {
     class Program
     {
         /*
-        Создайте приложение, которое позволит искать ин формацию в поисковой системе. 
-        Пользователь вводит текст для поиска, приложение отображает информацию 
-        с первой страницы результатов поиска. В качестве поис ковой системы можно 
-        использовать любую систему на ваш выбор. Например, Google или Bing
+        Создайте приложение, которое позволит искать изо
+        бражения в поисковой системе. Пользователь вводит 
+        текст для поиска изображения, приложение отображает 
+        картинки с первой страницы результатов поиска. В ка
+        честве поисковой системы можно использовать любую 
+        систему на ваш выбор. Например, Google или Bing.
         */
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Enter search query:");
-            string searchTerm = Console.ReadLine();
-            string url = $"https://www.google.com/search?q={Uri.EscapeDataString(searchTerm)}";
+            Console.WriteLine("Enter search query for img:");
+            string query = Console.ReadLine();
+            string apiKey = "";
+            string searchId = "";
+            string apiUrl = $"https://customsearch.googleapis.com/customsearch/v1?cx={searchId}&key={apiKey}&q={query}&searchType=image&start=0";
             try{
-                using (HttpClient client = new HttpClient())
+                using (var client = new HttpClient())
                 {
-                    var response = await client.GetStringAsync(url);
-
-                    var htmlDoc = new HtmlDocument();
-                    htmlDoc.LoadHtml(response);
-                    var results = htmlDoc.DocumentNode.SelectNodes("//div[@class='g']");
-                    if (results != null)
+                    var response = await client.GetAsync(apiUrl);
+                    if (response.IsSuccessStatusCode)
                     {
-                        foreach (var result in results)
+                        var content = await response.Content.ReadAsStringAsync();
+                        JObject jResponse = JObject.Parse(content);
+                        foreach (var result in jResponse["items"])
                         {
-                            var titleNode = result.SelectSingleNode(".//h3");
-                            var linkNode = result.SelectSingleNode(".//a");
-                            var snippetNode = result.SelectSingleNode(".//span[@class='aCOpRe']");
-                            if (titleNode!= null && linkNode!= null && snippetNode!= null)
-                            {
-                                string title = titleNode.InnerText;
-                                string link = linkNode.GetAttributeValue("href", "");
-                                string snippet = snippetNode.InnerText;
-                                Console.WriteLine($"Title: {title}");
-                                Console.WriteLine($"Link: {link}");
-                                Console.WriteLine($"Snippet: {snippet}");
-                                Console.WriteLine("--------------------------------");
-                            }
+                            Console.WriteLine($"Title: {result["title"]}");
+                            Console.WriteLine($"Link: {result["link"]}");
+                            Console.WriteLine($"Snippet: {result["snippet"]}");
+                            Console.WriteLine();
                         }
                     }
-                    else{
-                        Console.WriteLine("No results found.");
+                    else
+                    {
+                        Console.WriteLine($"Error: {response.StatusCode}");
                     }
                 }
-            }
-            catch (HttpRequestException ex)
-            {
-                Console.WriteLine($"Error connecting to the API: {ex.Message}");
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Error: {ex.Message}");
             }
+            
         }
     }
 }
