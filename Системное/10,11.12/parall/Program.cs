20cfa7551286287963e6eb3dccc7c336ad7ebf85
Лисёныш <71526719+Lisenich@users.noreply.github.com>
2026-01-11 19:09:56 +0300

    Orlov Eduard Program.cs
    Orlov Eduard Program.cs (11.01.2026)

--- Системное/10,11.12/parall/Program.cs
+++ Системное/10,11.12/parall/Program.cs
@@ -1,38 +1,87 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
-class Program
+namespace Parallel_and_PLINQ
 {
-	static void Main()
-	{
-		// ШАГ 1. Подготовка данных
-		// Создаём коллекцию чисел от 1 до 10 000 000.
-		// Пока мы просто подготавливаем данные, без вычислений.
-		List<int> numbers = new List<int>(10_000_000);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Numbers = new List<int>();
+            Random random = new Random();
+            int TotalResult = 0;
+            Stopwatch stopwatch = new Stopwatch();
 
-		for (int i = 1; i <= 10_000_000; i++)
-		{
-			numbers.Add(i);
-		}
+            Console.Write("Массив чисел: ");
 
-		// Проверка, что данные действительно созданы
-		Console.WriteLine($"Количество элементов: {numbers.Count}");
-		Console.WriteLine("Подготовка данных завершена.");
-	}
+            for (int i = 0; i < 100; i++)
+            {
+                int numberToAdd = random.Next(1, 10000000);
+                Numbers.Add(numberToAdd);
+                Console.Write(numberToAdd + "; ");
+            }
+            Console.WriteLine();
 
-	// ШАГ 1. Метод тяжёлого вычисления (CPU-bound)
-	static int HeavyCalculation(int x)
-	{
-		// Локальная переменная, не разделяется между потоками
-		int result = 0;
+            Console.WriteLine("\nПример обычного вычисления:");
+            stopwatch.Start();
+            foreach (int Number in Numbers)
+            {
+                TotalResult += Number;
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"Результат = {TotalResult}");
+            Console.WriteLine($"Время выполнения: {stopwatch.Elapsed}"); //в легких задачах будет быстрее параллельных вариантов
+            TotalResult = 0;
 
-		// Искусственно нагружаем процессор
-		for (int i = 0; i < 100; i++)
-		{
-			result += (x * i) % 7;
-		}
+            Console.WriteLine("\nПример неправильного параллельного вычисления:");
+            stopwatch.Start();
+            Parallel.ForEach(Numbers, number =>
+            {
+                TotalResult += number;
+            });
+            stopwatch.Stop();
+            Console.WriteLine($"Результат = {TotalResult}");
+            Console.WriteLine($"Время выполнения: {stopwatch.Elapsed}");
+            TotalResult = 0;
 
-		// Возвращаем результат вычисления
-		return result;
-	}
+            Console.WriteLine("\nПример правильного параллельного вычисления (с локальными аккумуляторами):");
+            stopwatch.Start();
+            Parallel.ForEach(Numbers,
+                () => 0,
+                (number, loopState, localResult) =>
+                                            {
+                                                localResult += number;
+                                                return localResult;
+                                            },
+                (localResult) =>
+            {
+                Interlocked.Add(ref TotalResult, localResult);  
+            });
+            stopwatch.Stop();
+            Console.WriteLine($"Результат = {TotalResult}");
+            Console.WriteLine($"Время выполнения: {stopwatch.Elapsed}");
+            TotalResult = 0;
+
+            Console.WriteLine("\nПример параллельного вычисления с помощью PLINQ:");
+            stopwatch.Start();
+            TotalResult = Numbers.AsParallel().Sum();
+            stopwatch.Stop();
+            Console.WriteLine($"Результат = {TotalResult}");
+            Console.WriteLine($"Время выполнения: {stopwatch.Elapsed}");
+            TotalResult = 0;
+
+            Console.WriteLine("\nПример параллельного вычисления с помощью PLINQ и с AsOrdered():");
+            stopwatch.Start();
+            //AsOrdered() немного замедляет выполнение функции, но обрабатывает источник данных, как упорядоченный
+            TotalResult = Numbers.AsParallel().AsOrdered().Sum();
+            stopwatch.Stop();
+            Console.WriteLine($"Результат = {TotalResult}");
+            Console.WriteLine($"Время выполнения: {stopwatch.Elapsed}");
+            TotalResult = 0;
+        }
+    }
 }
