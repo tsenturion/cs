48eda90811a0ee8e0b2ad79e5463bcc4e214545e
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2026-01-13 19:11:02 +0500

    first dll

---
+++ Системное/13.01/ClassLibrary1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+	public class Class1
+	{
+		public string GetMessage()
+		{
+			return "Hello from ClassLibrary1!";
+		}
+
+		public int Calculate(int a, int b)
+		{
+			return a + b;
+		}
+	}
+}
